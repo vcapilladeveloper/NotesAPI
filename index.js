e904ccde73a3b11853622b763abc34f98965bd8e
Victor Capilla <victorcapilladeveloper@gmail.com>
2024-08-16 13:48:10 +0200

    add dontenv

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
@@ -64,7 +65,7 @@ app.delete('/api/notes/:id', (request, response) => {
     response.status(204).end()
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })

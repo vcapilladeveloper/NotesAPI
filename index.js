c6935c7a0c0044455a377d07f4140c768c68fc65
Victor Capilla <victorcapilladeveloper@gmail.com>
2024-08-17 16:47:29 +0200

    Add Error handling into Middleware

--- index.js
+++ index.js
@@ -10,20 +10,20 @@ app.use(cors())
 
 app.post('/api/notes', (request, response) => {
     const body = request.body
-  
+
     if (body.content === undefined) {
-      return response.status(400).json({ error: 'content missing' })
+        return response.status(400).json({ error: 'content missing' })
     }
-  
+
     const note = new Note({
-      content: body.content,
-      important: body.important || false,
+        content: body.content,
+        important: body.important || false,
     })
-  
+
     note.save().then(savedNote => {
-      response.json(savedNote)
+        response.json(savedNote)
     })
-  })
+})
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
@@ -35,24 +35,60 @@ app.get('/api/notes', (request, response) => {
     })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
+app.get('/api/notes/:id', (request, response, next) => {
+    console.log(request.params.id)
+    Note.findById(request.params.id)
+        .then(note => {
+            if (note) {
+                response.json(note)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
+})
 
-    if (note) {
-        response.json(note)
-    } else {
-        response.status(404).end()
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+
+    const note = {
+        content: body.content,
+        important: body.important,
     }
+
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+        .then(updatedNote => {
+            response.json(updatedNote)
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
 
-    response.status(204).end()
-})
+app.use(unknownEndpoint)
 
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+// este debe ser el último middleware cargado, ¡también todas las rutas deben ser registrada antes que esto!
+app.use(errorHandler)
 const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
